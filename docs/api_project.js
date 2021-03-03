085fcd461fbe2e48a08fb1eb5bdacf87cb1e931f
test <ayan.tahir@invozone.com>
2021-03-03 18:10:40 +0500

    docs added

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "real-state-project",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-03-03T13:10:04.218Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});
